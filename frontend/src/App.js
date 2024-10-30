3c55647487c97898b67b72d7bfe1873eb1f2f8d8
Hamza <nuruzadahamza@gmail.com>
2024-10-30 14:53:55 +0400

    anani sildim

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage'; 
-
-// import './assets/scss/main.scss';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import HomePage from './pages/HomePage';
+import './assets/scss/main.scss';  
 
 const App = () => {
   return (
