a975ae02896dddf1748afb40b5c59caa246107e5
Hamza <nuruzadahamza@gmail.com>
2024-11-15 20:23:34 +0400

    1 hour

--- src/contexts/PredictionContext.js
+++ src/contexts/PredictionContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react';
 const PredictionContext = createContext();
 
 export const PredictionProvider = ({ children }) => {
-    const [interval, setInterval] = useState('1M');
+    const [interval, setInterval] = useState('1H');
     const [duration, setDuration] = useState(1);
 
     return (
